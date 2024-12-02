f0a3952b7154b1908b514c08cc675b2ec036c565
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-12-02 18:59:42 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,7 +49,7 @@ const Login = async(req, res) => {
        const token = generateToken(user);
 
        res.cookie('authToken', token , {
-        httpOnly : true,
+        httpOnly : false,
         secure :  process.env.NODE_ENV === 'production'
        })
 
