6332930652861a39502039b46db86b9867b47bee
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-12-05 23:33:44 +0700

    Update laporanModels.js

--- models/laporanModels.js
+++ models/laporanModels.js
@@ -29,7 +29,10 @@ const laporanSchema = new Schema({
         type: String,
         required: true
     },
-    gambar_pendukung: [String] 
+    gambar_pendukung: {
+        type: [String],
+        default: ['https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hops.id%2Funik%2F29410689482%2Fmengenal-kepribadian-dari-foto-profil-di-media-sosial-memajang-foto-artis-korea-ternyata-berarti-ini&psig=AOvVaw0PcC2IOouKKr6u3ssWcRBZ&ust=1733502756845000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjSgJSHkYoDFQAAAAAdAAAAABAE'],
+    }
 });
 
  
