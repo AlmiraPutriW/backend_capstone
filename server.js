64a493d1a01299ff6da048023fb3c342874cebe5
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-12-17 20:49:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,21 +22,9 @@ cloudinary.config({
 
 const cors = require('cors');
 
-// Daftar origins yang diperbolehkan
-const allowedOrigins = [
-  'http://localhost:8081',
-  'https://sipraja-capstone.netlify.app'
-];
-
-// Middleware CORS
+// Middleware
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Izinkan jika origin sesuai daftar
-    } else {
-      callback(new Error('Origin not allowed by CORS')); // Tolak jika tidak ada di daftar
-    }
-  },
+  origin: 'http://localhost:8081', 
   credentials: true, // Izinkan cookie lintas origin
 }));
 
